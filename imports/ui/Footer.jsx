6ed2287d80771b81423c3f4133172d1c503e6199
Liam Jongsu Kim <appleparan@gmail.com>
2016-10-18 23:42:32 +0900

    change footer design

--- imports/ui/Footer.jsx
+++ imports/ui/Footer.jsx
@@ -8,18 +8,26 @@ export default class FooterInstance extends Component {
         <footer>
           <a name="Contact" className="anchor"></a>
           <Row>
-            <p>&copy; Haeeun Han 2016</p>
-            <address>
-              <strong>MEDVIEWSOFT</strong><br />
-              50 Yonsei-ro, Seodaemun-gu<br />
-              Seoul, Korea 03722<br />
-              <abbr title="Phone">P:</abbr> (82)2 2123-6121
-            </address>
+            <Col xs={12} sm={6}>
+              <p> <strong>Event manager</strong> </p>
+              <p>
+                Haeeun Han <br /> 
+                <i className="fa fa-phone" aria-hidden="true"></i> (82) 2 2123-6121 <br />
+                <i className="fa fa-envelope-o" aria-hidden="true"></i> <a href="mailto:csedept@yonsei.ac.kr"> csedept@yonsei.ac.kr</a>
+              </p>
+            </Col>
 
-            <address>
-              <strong>Haeeun Han</strong>  Event manager<br />
-              <a href="mailto:#">csedept@yonsei.ac.kr</a>
-            </address>
+            <Col xs={12} sm={6}>
+              <p>&copy; Haeeun Han 2016</p>
+              <address>
+                <strong>MEDVIEWSOFT</strong><br />
+                50 Yonsei-ro, Seodaemun-gu<br />
+                Seoul, Korea, 03722<br />
+                <i className="fa fa-link" aria-hidden="true"></i> <a href="http://mediviewsoft.com/"> homepage </a> <br />
+                <i className="fa fa-phone" aria-hidden="true"></i> (82) 2 2123-6121 <br />
+                <i className="fa fa-envelope-o" aria-hidden="true"></i> <a href="mailto:csedept@yonsei.ac.kr"> csedept@yonsei.ac.kr</a>
+              </address>
+            </Col>
           </Row>
         </footer>
       </Grid>
