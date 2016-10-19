f0c1df234fd8c25f4e83ebb2eed778cf990fc443
Liam Jongsu Kim <appleparan@gmail.com>
2016-10-19 13:31:19 +0900

    change navbar structure

--- imports/ui/Navbar.jsx
+++ imports/ui/Navbar.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Navbar, Nav, NavItem } from 'react-bootstrap';
+import { Navbar, Nav, NavItem, NavDropdown, MenuItem, Button } from 'react-bootstrap';
 
 export default class NavbarInstance extends React.Component {
   render() {
@@ -13,13 +13,19 @@ export default class NavbarInstance extends React.Component {
         </Navbar.Header>
         <Navbar.Collapse>
           <Nav pullRight>
-            <NavItem eventKey={2} href="#organizers">Organizers</NavItem>
-            <NavItem eventKey={3} href="#speakers">Session Chair</NavItem>
-            <NavItem eventKey={4} href="#schedule">Schedule</NavItem>
-            <NavItem eventKey={5} href="#registration">Registration</NavItem>
-            <NavItem eventKey={6} href="#venue">Venue</NavItem>
-            <NavItem eventKey={7} href="#accommodation">Accommodation</NavItem>
-            <NavItem eventKey={8} href="#transportation">Travel &amp; Transportation </NavItem>
+            <NavDropdown eventKey={1} title="Program" id="navbar-drop-speaker">
+              <MenuItem eventKey={1.1} href="#organizers">Organizers</MenuItem>
+              <MenuItem eventKey={1.2} href="#speakers">Speakers</MenuItem>
+              <MenuItem eventKey={1.2} href="#venue">Venue</MenuItem>
+            </NavDropdown>
+            <NavItem eventKey={2} href="#schedule">Schedule</NavItem>
+            <NavDropdown eventKey={3} title="Info" id="navbar-drop-accomm">
+              <MenuItem eventKey={3.1} href="#accommodation">Accommodation</MenuItem>
+              <MenuItem eventKey={3.2} href="#travel">Travel</MenuItem>
+            </NavDropdown>
+            <Navbar.Form pullLeft>
+              <Button bsStyle="primary" href="#registration">Register Now </Button>
+            </Navbar.Form>
           </Nav>
         </Navbar.Collapse>
       </Navbar>
