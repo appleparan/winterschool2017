9835715a7ea5c73dc6e8e3c230616a424208c4c9
Liam Jongsu Kim <appleparan@gmail.com>
2016-10-28 07:33:20 +0900

    fix IndexLinkContainer - I can't fix another one because react-bootstrap doesn't have Navbar.Button https://github.com/react-bootstrap/react-router-bootstrap/issues/185 change menuItem name : Organizer -> Program

--- imports/ui/containers/Navbar.jsx
+++ imports/ui/containers/Navbar.jsx
@@ -2,26 +2,25 @@ import React, { Component } from 'react';
 import { render } from 'react-dom';
 
 import { Navbar, Nav, NavItem, NavDropdown, MenuItem, Button } from 'react-bootstrap';
+import { IndexLink, Link } from 'react-router';
 import { IndexLinkContainer, LinkContainer } from 'react-router-bootstrap';
 
 // For IndexLink & LinkContainer
 // https://themeteorchef.com/snippets/react-router-basics/#tmc-setting-up-our-routes
 export const NavbarLayout = () => (
   <Navbar fixedTop>
-    <IndexLinkContainer to="/">
-      <Navbar.Header>
-        <Navbar.Brand>
-        <IndexLinkContainer to="/">
-          <a>2017 Winter School</a>
-        </IndexLinkContainer>
-        </Navbar.Brand>
-        <Navbar.Toggle />
-      </Navbar.Header>
-    </IndexLinkContainer>
+    <Navbar.Header>
+      <Navbar.Brand>
+        <IndexLink to="/">
+          2017 Winter School
+        </IndexLink>
+      </Navbar.Brand>
+      <Navbar.Toggle />
+    </Navbar.Header>
     <Navbar.Collapse>
       <Nav pullRight>
         <LinkContainer to="/program">
-          <NavItem title="Program" eventKey="1">Organizers</NavItem>
+          <NavItem title="Program" eventKey="1">Program</NavItem>
         </LinkContainer>
         <LinkContainer to="/schedule">
           <NavItem title="Schedule" eventKey="2">Schedule</NavItem>
@@ -29,11 +28,9 @@ export const NavbarLayout = () => (
         <LinkContainer to="/venue">
           <NavItem title="Venue" eventKey="3">Venue</NavItem>
         </LinkContainer>
-        <Navbar.Form pullLeft>
-          <LinkContainer to="/registernow">
-            <Button  eventKey="4" bsStyle="primary"> Register Now </Button>
-          </LinkContainer>
-        </Navbar.Form>
+        <LinkContainer to="/register">
+          <Button  title="Register" eventKey="4" bsStyle="primary" className="navbar-btn"> Register Now </Button>
+        </LinkContainer>
       </Nav>
     </Navbar.Collapse>
   </Navbar>
