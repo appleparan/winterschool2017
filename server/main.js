7c200d4762ae27be6a0276cb16aa1348eee99802
Liam Jongsu Kim <appleparan@gmail.com>
2016-11-07 04:27:18 +0900

    import mailgun account information (do not add mailgun info to github)

--- server/main.js
+++ server/main.js
@@ -1,8 +1,13 @@
 import { Meteor } from 'meteor/meteor';
+import { check } from 'meteor/check';
+// get mailgun settings
+import { MailURL } from '/imports/startup/server/mail-url.js';
 
 // import '/imports/startup/server';
 // import '/imports/startup/both';
 
 Meteor.startup(() => {
   // code to run on server at startup
+  check(MailURL, String);
+  process.env.MAIL_URL = MailURL;
 });
