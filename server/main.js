5a0b65bdd7ecd3945ee4850204dbfc5b562d9440
Haeeun Han <aclearautumnday@gmail.com>
2016-11-19 13:01:21 +0900

    do not get environmental variable from files

--- server/main.js
+++ server/main.js
@@ -2,11 +2,7 @@ import { Meteor } from 'meteor/meteor';
 import { check } from 'meteor/check';
 // get mailgun settings
 import '/imports/startup/server/index.jsx';
-import { MailURL } from '/imports/startup/server/settings/mail-url.js';
-import { iamportKey, iamportSecret } from '/imports/startup/server/settings/iamport.js';
 
 Meteor.startup(() => {
   // code to run on server at startup
-  check(MailURL, String);
-  process.env.MAIL_URL = MailURL;
 });
