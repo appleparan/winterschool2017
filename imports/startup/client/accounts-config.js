1688729ab4862dd8c03b069e9fe03475ce8060a9
Liam Jongsu Kim <appleparan@gmail.com>
2016-11-21 09:13:02 +0900

    add callback after signup and signin

--- imports/startup/client/accounts-config.js
+++ imports/startup/client/accounts-config.js
@@ -1,6 +1,6 @@
 import React from 'react';
-// import { Accounts } from 'meteor/std:accounts-bootstrap';
 import { Accounts } from 'meteor/std:accounts-ui';
+import { browserHistory } from 'react-router';
 
 Accounts.config({
   sendVerificationEmail: true,
@@ -11,9 +11,10 @@ Accounts.config({
 Accounts.ui.config({
   passwordSignupFields: 'EMAIL_ONLY_NO_PASSWORD',
   loginPath: '/signin',
+  onSignedInHook: () => browserHistory.push('/register'),
+  onPostSignUpHook: () => browserHistory.push('/register'),
   signUpPath: '/signup',
   profilePath: '/profile',
   requireEmailVerification: true,
   minimumPasswordLength: 8
-
 });
